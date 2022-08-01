19a41da62860bda4e9e47ed936f07d013a137b67
Amanda Moore <amanda.joy24@outlook.com>
2022-08-01 11:41:59 -0700

    wait for application to actually close

--- scripts/auto/stop.sh
+++ scripts/auto/stop.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 main() {
     screen -S webapp -X quit
+    local cnt=0
+    while (( cnt -le 15 ))
+    do
+        if [ -f updater.lock ]
+        then
+            sleep 1
+        else
+            break
+        fi
+    done
+    rm -f updater.lock
 }
 
 main
